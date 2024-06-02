41db3dcb13b29e5baffdcee362b9e1c61fd2dc5e
Stracker-Maxime <maxime@stracker.tech>
2024-06-02 22:54:08 +0200

    EVO - (bid) : Get bid list in front end

--- front/pages/auctions/[id].js
+++ front/pages/auctions/[id].js
@@ -11,10 +11,12 @@ const AuctionPage = () => {
   const auctionId = router.query.id;
 
   const [auctionValue, setAuctionValue] = useState([]);
+  const [bidList, setBidList] = useState([]);
   const [timeLeft, setTimeLeft] = useState("");
 
   useEffect(() => {
     auctionId && getAuctionData();
+    auctionId && getAuctionBidList();
   }, [auctionId]);
 
   const getAuctionData = async () => {
@@ -24,14 +26,37 @@ const AuctionPage = () => {
     setAuctionValue(auctionData);
   };
 
+  const getAuctionBidList = async () => {
+    const bidList = await api(`http://localhost:8000/bid/list/${auctionId}`);
+    setBidList(bidList);
+  };
+
   return (
     <main className="flex">
       <Header />
-      <div className="mt-8 ml-8">
-        <h1>{auctionValue.title}</h1>
-        <p>{auctionValue.description}</p>
-        <p>Prix de réserve : {auctionValue.reservePrice}€</p>
-        <Countdown expiration={auctionValue.expiration} timeLeft={timeLeft} setTimeLeft={setTimeLeft}/>
+      <div className="flex mt-8 ml-8 w-full">
+        <div className="w-1/2">
+          <h1>{auctionValue.title}</h1>
+          <p>{auctionValue.description}</p>
+          <p>Prix de réserve : {auctionValue.reservePrice}€</p>
+          <Countdown
+            expiration={auctionValue.expiration}
+            timeLeft={timeLeft}
+            setTimeLeft={setTimeLeft}
+          />
+        </div>
+        <div className="w-1/2">
+          <h1>Bid List :</h1>
+          <div>
+            <ul>
+              {bidList && bidList.length > 0 ? (
+                bidList.map((bid, index) => <li key={index}>{bid.amount} $</li>)
+              ) : (
+                <li>No bid for this auction!</li>
+              )}
+            </ul>
+          </div>
+        </div>
       </div>
     </main>
   );
