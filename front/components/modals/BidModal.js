63d1fffa66c2f868da44bf6ef8a9a6329fed8540
Stracker-Maxime <maxime@stracker.tech>
2024-06-04 07:51:19 +0200

    EVO - (modal) : Add redirect button when jwt expire

--- front/components/modals/BidModal.js
+++ front/components/modals/BidModal.js
@@ -10,9 +10,11 @@ const BidModal = ({ setShowModal }) => {
     if (token) {
       try {
         const decoded = jwt.verify(token, "My_secret");
-        console.log(decoded)
+
+        // ajouter form d'envoi bid
       } catch (err) {
         console.log(err);
+        setDisplayLogin(true);
       }
     } else {
       setDisplayLogin(true);
@@ -34,10 +36,20 @@ const BidModal = ({ setShowModal }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center">
-      <div className="w-1/4 h-1/2 rounded-2xl border-2" ref={modalRef}>
-        Modal
+      <div className="w-1/4 h-1/2 rounded-2xl border-2 p-4" ref={modalRef}>
+        Enchérire
         {displayLogin && (
-          <p className="text-red">Vous n'êtes pas authentifié</p>
+          <div className="flex flex-col items-center mt-4">
+            <p className>Vous n'êtes pas authentifié</p>
+            <div className="w-full flex justify-between px-8 mt-6">
+              <a href="/user/login">
+                <button className="border-2">Login</button>
+              </a>
+              <a href="/user/register">
+                <button className="border-2">Register</button>
+              </a>
+            </div>
+          </div>
         )}
       </div>
     </div>
